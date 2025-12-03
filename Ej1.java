6c08b221779f2cd660f64b185c4565073868cb9e
daryjrr <dariofdezrubio@gmail.com>
2025-12-03 21:13:45 +0100

    Add files via upload

---
+++ Ej1.java
@@ -0,0 +1,66 @@
+package examen;
+import java.util.Random;
+import java.util.Scanner;
+
+
+class dniInvalidoException extends Exception {
+    public dniInvalidoException(String mensaje) {
+        super(mensaje);
+    }
+}
+
+public class Ej1 {	
+
+static String leerDni() {
+	
+	Scanner sc =new Scanner (System.in);
+	System.out.print("Introduce un DNI (8 números y una letra): ");
+	String dni = sc.next();
+	
+	if (dni.length() !=9 || !dni.matches("^[0-9]{8}[a-zA-Z]$")) {
+		try {
+			throw new dniInvalidoException ("DNI inválido: debe tener 8 números y una letra.");
+		} catch (dniInvalidoException e) {
+			System.out.println(e.getMessage());
+		}
+		return leerDni();
+	}
+	
+	return dni;
+}
+	
+
+
+static String cambiarDigito(String dni, int posicion, char nuevoValor) {
+	
+	char letra= Character.toUpperCase(dni.charAt(8));
+	
+	if (!Character.isDigit(nuevoValor)) {
+		System.out.println("Error: el nuevo valor debe ser un dígito.");
+		return dni;
+	}
+	else {
+		char[] dniArray = dni.toCharArray();
+		int numero = posicion - 1;
+		
+		if (numero <0 || numero > 7) {
+			System.out.println("Posición inválida (1-8).");
+			return dni;
+		}
+		dniArray[numero]= nuevoValor;
+		dniArray[8] =letra;
+		
+		return new String (dniArray);
+	}
+}
+
+
+
+	public static void main(String[] args) {
+		String dni = leerDni();
+		System.out.println("DNI original: " + dni);
+		
+		String dniNuevo = cambiarDigito(dni, 1, '1');
+		System.out.println("DNI modificado: " + dniNuevo);
+	}
+}
---
+++ Ej2.java
@@ -0,0 +1,72 @@
+package examen;
+import java.util.Scanner;
+
+public class Ej2 {
+static void rellenaEncuesta(String[][] encuesta) {
+	Scanner sc = new Scanner(System.in);
+
+	for (int i = 0; i < encuesta.length; i++) {
+		System.out.println("Introduce código postal (o 'fin' para terminar): ");
+		String codigoPostal = sc.nextLine().toLowerCase();
+		if (codigoPostal.equals("fin")) {
+			break;
+		}
+		System.out.println("Introduce edad: ");
+		String edad = sc.nextLine();
+		System.out.println("Introduce sexo (M/F): ");
+		String sexo = sc.nextLine().toUpperCase();
+		
+		encuesta[i][0] = codigoPostal;
+		encuesta[i][1] = edad;
+		encuesta[i][2] = sexo;
+		
+}
+}
+
+static void muestraResultados(String[][] encuesta) {
+	System.out.println("--- Resultados de la encuesta ---");
+	for (int i = 0; i < encuesta.length; i++) {
+		if (encuesta[i][0] == null) {
+			break;
+		}
+		System.out.println("Código Postal: " + encuesta[i][0] + ", Edad: " + encuesta[i][1] + ", Sexo: " + encuesta[i][2]);
+	}
+}
+
+static int calculaMediaEdad(String[][] encuesta, char sexo, int codigoPostal) {
+	int suma = 0;
+	int contador = 0;
+	
+	for (int i = 0; i < encuesta.length; i++) {
+		int codigo = Integer.parseInt(encuesta[i][0]);
+		char sex = encuesta[i][2].charAt(0);
+		
+		if (codigo == codigoPostal && sex == sexo) {
+			suma += Integer.parseInt(encuesta[i][1]);
+			contador++;
+		}
+	}
+			return suma / contador;
+			}
+
+
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	String[][] encuesta = new String[500][3];
+	
+	rellenaEncuesta(encuesta);
+	muestraResultados(encuesta);
+	
+	System.out.print("Introduce código postal para calcular media de edad: ");
+	int codigoPostal = sc.nextInt();
+	System.out.print("Introduce sexo (M/F) para calcular media de edad: ");
+	char sexo = sc.next().toUpperCase().charAt(0);
+	
+	int mediaEdad = calculaMediaEdad(encuesta, sexo, codigoPostal);
+	
+	System.out.println("La media de edad para el código postal " + codigoPostal + " y sexo " + sexo + " es: " + mediaEdad);
+	
+}
+}
+
+
